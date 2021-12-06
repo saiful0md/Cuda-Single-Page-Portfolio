2c1069d988f33fa471d001a940fbfe635ff06900
saiful0md <saiful331834@gmail.com>
2021-12-06 17:46:44 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    // sticky menu
+    $(".js--services-section").waypoint(function(direction){
+        if (direction == "down"){
+            $("nav").addClass("sticky");
+        } else{
+            $("nav").removeClass("sticky");
+        }
+    });
+    
+    // mixitup (portfolio)
+    var mixer = mixitup('.filter-mixer');
+});
+
+
